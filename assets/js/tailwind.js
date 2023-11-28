abd66594fbf8ee68d7ed4fd16467774024ab3b11
Guilherme Moreira <guilherme.moreira@compasso.com.br>
2023-11-28 10:41:32 -0300

    add crlf in images svg

---
+++ assets/js/tailwind.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        colors: {
+            brand: {
+                white: "#F9EBFF",
+                black: "#0F010C",
+                beige: "#E28E7D",
+                pink: "#89093D",
+                marsala: "#49072A",
+                ocean: "#3E90BA",
+                sky: "#83CCE5",
+            },
+        },
+    },
+};
