02718d31f501d5a85f3228a1a73d3f8896a05560
Guilherme Moreira <guilhermemoreira.web@gmail.com>
2023-11-29 19:15:21 -0300

    HTML DONE

--- assets/js/tailwind.js
+++ assets/js/tailwind.js
@@ -1,15 +1,24 @@
 tailwind.config = {
-    theme: {
-        colors: {
-            brand: {
-                white: "#F9EBFF",
-                black: "#0F010C",
-                beige: "#E28E7D",
-                pink: "#89093D",
-                marsala: "#49072A",
-                ocean: "#3E90BA",
-                sky: "#83CCE5",
-            },
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          white: "#F9EBFF",
+          black: "#0F010C",
+          beige: "#E28E7D",
+          pink: "#89093D",
+          marsala: "#49072A",
+          ocean: "#3E90BA",
+          sky: "#83CCE5",
         },
+      },
+      backgroundImage: {
+        "hero-texture": "url('/assets/img/hero-pattern.png')",
+        sacada: "url('/assets/img/sacada.png')",
+      },
+      backgroundSize: {
+        5: "5%",
+      },
     },
+  },
 };
