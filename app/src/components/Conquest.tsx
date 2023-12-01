1d7048bf6099f9d9ea894294b3ea3290d7220b2c
Guilherme Moreira <guilhermemoreira.web@gmail.com>
2023-12-01 20:56:24 -0300

    add Conquest Component

---
+++ app/src/components/Conquest.tsx
@@ -0,0 +1,61 @@
+export default function ConquestComponent() {
+  return (
+    <section className="w-full flex flex-col justify-center items-center gap-8 p-4 text-brand-marsala">
+      <h2 className="font-extrabold text-3xl text-center" data-aos="fade-down">
+        O que você vai conquistar se adquirir o e-book agora?
+      </h2>
+      <div className="grid grid-cols-2 gap-4 m-auto">
+        <p
+          className="max-w-sm font-medium"
+          data-aos="fade-right"
+          data-aos-delay="100"
+        >
+          🕒 Tempo é Precioso - Receitas Rápidas
+        </p>
+        <p
+          className="max-w-sm font-medium"
+          data-aos="fade-right"
+          data-aos-delay="200"
+        >
+          📅 Planejamento de Refeições
+        </p>
+        <p
+          className="max-w-sm font-medium"
+          data-aos="fade-right"
+          data-aos-delay="300"
+        >
+          🌮 Variedade para Paladares Exigentes
+        </p>
+        <p
+          className="max-w-sm font-medium"
+          data-aos="fade-right"
+          data-aos-delay="400"
+        >
+          🛒 Listas de Compras Descomplicadas
+        </p>
+        <p
+          className="max-w-sm font-medium"
+          data-aos="fade-right"
+          data-aos-delay="500"
+        >
+          🌿 Nutrição sem Compromissos
+        </p>
+        <p
+          className="max-w-sm font-medium"
+          data-aos="fade-right"
+          data-aos-delay="600"
+        >
+          🍽️ Simplicidade na Cozinha - Zero Estresse
+        </p>
+        <p
+          className="max-w-sm font-medium"
+          data-aos="fade-right"
+          data-aos-delay="700"
+        >
+          🎉 Liberte-se da Pressão na Cozinha e Descubra o Prazer de Cada
+          Refeição!
+        </p>
+      </div>
+    </section>
+  );
+}
