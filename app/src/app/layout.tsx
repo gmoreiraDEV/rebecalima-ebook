cdb72c47f3b68c10ce750a8fc53894028d020b3d
Guilherme Moreira <guilhermemoreira.web@gmail.com>
2023-12-01 20:56:08 -0300

    add footer

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -20,6 +20,38 @@ export default function RootLayout({
         className={`${inter.className} bg-brand-white text-brand-black transition-all ease-in-out`}
       >
         {children}
+        <footer className="w-full flex justify-between items-center min-h-24 py-4 bg-brand-black/80 mt-12">
+          <div className="w-full lg:w-2/3 m-auto flex flex-col lg:flex-row justify-between items-center text-brand-white gap-4">
+            <div className="flex flex-col items-center justify-center lg:flex-row gap-4">
+              <img
+                src="/img/vlnt.svg"
+                alt="Logo Valente - Consultoria &amp; Inovação"
+                className="max-w-[200px]"
+              />
+              <div className="text-center lg:text-left group">
+                <strong className="flex justify-between items-center">
+                  Copyright © 2023 Valente Consultoria e Inovação LTDA
+                  <span className="opacity-0 group-hover:opacity-100 font-normal">
+                    &nbsp;/ CNPJ: 31.803.058/0001-05
+                  </span>
+                </strong>
+                <p>
+                  Av. Paulista, 1471 - Conj 1110, Jd. Paulista - São Paulo - SP,
+                  01311-927
+                </p>
+              </div>
+            </div>
+            <div>
+              <a href="https://basixdigital.com.br">
+                <img
+                  src="/img/x-basix.svg"
+                  alt="Basix Digital"
+                  className="w-11 h-11"
+                />
+              </a>
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
   );
