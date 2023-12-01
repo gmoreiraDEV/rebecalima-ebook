ac54791d07686c9abce85c4dd6cd533923885bae
Guilherme Moreira <guilhermemoreira.web@gmail.com>
2023-12-01 20:57:54 -0300

    add AOS animation library

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import ConquestComponent from "@/components/Conquest";
+import ContentComponent from "@/components/Content";
+import EssentialComponent from "@/components/Essential";
 import HeroComponent from "@/components/Hero";
+import IdealForYouComponent from "@/components/IdealForYou";
+import RebecaLimaComponent from "@/components/RebecaLima";
+import SaleComponent from "@/components/Sale";
+import ValueComponent from "@/components/Value";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <main className="flex flex-col items-center gap-8 w-full">
       <HeroComponent />
+      <EssentialComponent />
+      <ContentComponent />
+      <ValueComponent />
+      <IdealForYouComponent />
+      <ConquestComponent />
+      <SaleComponent />
+      <RebecaLimaComponent />
+      <ValueComponent />
     </main>
   );
 }
