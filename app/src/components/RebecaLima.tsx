b5bbfcb5672bfeea07a58d1c1820ea2c542ceeb1
Guilherme Moreira <guilhermemoreira.web@gmail.com>
2023-12-08 17:25:15 -0300

    feat: add constants in file

--- app/src/components/RebecaLima.tsx
+++ app/src/components/RebecaLima.tsx
@@ -1,3 +1,5 @@
+import { INSTAGRAM } from "@/constants";
+
 export default function RebecaLimaComponent() {
   return (
     <section
@@ -6,8 +8,8 @@ export default function RebecaLimaComponent() {
     >
       <div>
         <a
-          className="p-2 bg-brand-black/80 backdrop-blur text-brand-white rounded-full border-2 border-solid border-brand-black"
-          href="http://instagram.com/rebecalimanutri"
+          className="p-2 bg-brand-black/80 backdrop-blur text-brand-white rounded-full border-2 border-solid border-brand-black hover:p-4 hover:text-brand-black hover:bg-brand-white"
+          href={INSTAGRAM}
           target="_blank"
           data-aos="fade-right"
           data-aos-delay="200"
@@ -24,9 +26,24 @@ export default function RebecaLimaComponent() {
           De Nutricionista a Chef de Cozinha. <br />
           Quem é <span className="text-brand-sky">Rebeca Lima</span>?
         </h2>
-        <p data-aos="fade-right" data-aos-delay="400">
-          Texto texto texto
-        </p>
+        <div className="text-brand-white max-w-lg">
+          <p className="mt-4" data-aos="fade-right" data-aos-delay="400">
+            Graduada em Nutrição e em Gastronomia, <strong>Rebeca Lima</strong>{" "}
+            é uma pessoa que une duas paixões,&nbsp;
+            <strong>Nutrição e Gastronomia</strong>.
+          </p>
+          <p className="mt-4" data-aos="fade-right" data-aos-delay="500">
+            Especializada em criar experiências gastronômicas personalizadas,
+            seu trabalho como <strong>Personal Chef</strong> consiste em ir até
+            seus clientes, cozinhando menus sob medida de acordo com as
+            preferências e necessidades nutricionais individuais.
+          </p>
+          <p className="mt-4" data-aos="fade-right" data-aos-delay="600">
+            Além da Nutrição, ela possui uma criatividade na cozinha
+            incomparável, sempre inovando suas preparaçãoes com novos sabores e
+            texturas.
+          </p>
+        </div>
       </div>
     </section>
   );
